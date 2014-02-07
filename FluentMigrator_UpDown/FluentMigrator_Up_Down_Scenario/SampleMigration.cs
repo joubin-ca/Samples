cc41ab7f6b19bdfd1806ad67f9a4f46d24bd8cb8
Joubin Najmaie <jnajmaie@objectsharp.com>
2014-02-07 09:21:42 -0500

    Sample FluentMigrator starter

---
+++ FluentMigrator_UpDown/FluentMigrator_Up_Down_Scenario/SampleMigration.cs
@@ -0,0 +1,32 @@
+﻿using FluentMigrator;
+
+namespace FMDB
+{
+    [Migration(1)]
+    public class SampleMigration_01 : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Table1").WithColumn("Id").AsInt32().PrimaryKey();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("Table1");
+        }
+    }
+
+    [Migration(2)]
+    public class SampleMigration_02 : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Table2").WithColumn("Id").AsInt32().PrimaryKey();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("Table2");
+        }
+    }
+}
